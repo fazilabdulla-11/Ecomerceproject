602f82de9883cb9910c16c015dee3cdee7ea83cc
fazil <fazilabdullatnr@gmail.com>
2026-02-25 13:09:16 +0530

    home page loaded

--- Ecomerceproject/urls.py
+++ Ecomerceproject/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from turtle import home
+
 
 from django.contrib import admin
 from django.urls import path
 
+from newapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home')
+    path('',views.home,name='home'),
     
 ]
